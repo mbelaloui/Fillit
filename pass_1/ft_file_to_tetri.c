8566008c97c0d216556c249490201ab19ba2116f
Mehdi aissa BELALOUI <mbelalou@e1r4p1.42.fr>
2017-12-20 18:18:28 +0100

    Final

---
+++ pass_1/ft_file_to_tetri.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_check_file.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbelalou <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/12 10:50:12 by mbelalou          #+#    #+#             */
+/*   Updated: 2017/12/20 17:07:14 by mbelalou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fillit_p_1.h"
+
+int		ft_file_to_tetri(t_tetri **begin, int nbr_tetrim, char buf[])
+{
+	int		i;
+	char	**mat;
+	char	c;
+	t_tetri	*list;
+	t_tetri	*temp_tetri;
+
+	list = NULL;
+	i = 0;
+	c = 'A';
+	if (nbr_tetrim <= 0 || buf == NULL)
+		return (0);
+	while (i < nbr_tetrim)
+	{
+		mat = ft_extract_tetri(buf, i++);
+		if (!ft_check_tetri(mat))
+			return (0);
+		if (!(temp_tetri = ft_newtetri(mat, c++)))
+			return (0);
+		if (!ft_addtetri(&list, temp_tetri))
+			return (0);
+		if (*begin == NULL)
+			*begin = list;
+	}
+	return (1);
+}
---
+++ pass_2/ft_put_tetri_mat.c
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_put_tetrim_mat.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbelalou <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/15 16:22:34 by mbelalou          #+#    #+#             */
+/*   Updated: 2017/12/20 18:09:05 by mbelalou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fillit_p_2.h"
+
+void	ft_add_tetri(char ***mat, t_tetri *t, int x, int y)
+{
+	int pt;
+
+	pt = 0;
+	while (pt < 4)
+	{
+		(*mat)[x + t->tab_x[pt]][y + t->tab_y[pt]] = t->c;
+		pt++;
+	}
+}
+
+void	ft_rm_tetri(char ***mat, t_tetri *t, int x, int y)
+{
+	int pt;
+
+	pt = 0;
+	while (pt < 4)
+	{
+		(*mat)[x + t->tab_x[pt]][y + t->tab_y[pt]] = '.';
+		pt++;
+	}
+}
+
+int		ft_is_possible(char **mat, t_tetri *t, int dim_mat, t_point *point)
+{
+	int pt;
+
+	if (!mat || !t || dim_mat < 2 || point->x < 0 || point->x > dim_mat
+			|| point->y < 0 || point->y > dim_mat)
+		return (0);
+	pt = 0;
+	while (pt < 4)
+	{
+		if (point->x + t->tab_x[pt] >= dim_mat)
+			return (0);
+		if (point->y + t->tab_y[pt] < 0 || point->y + t->tab_y[pt] >= dim_mat)
+			return (0);
+		if (mat[point->x + t->tab_x[pt]][point->y + t->tab_y[pt]] != '.')
+			return (0);
+		pt++;
+	}
+	return (1);
+}
+
+int		ft_put_tetri_mat(char ***mat, t_tetri *t, int dim_mat)
+{
+	t_point *point;
+
+	if (!(point = malloc(sizeof(t_point))))
+		return (0);
+	point->x = -1;
+	while (point->x++ < dim_mat && t)
+	{
+		point->y = -1;
+		while (point->y++ < dim_mat && t)
+		{
+			if (ft_is_possible(*mat, t, dim_mat, point))
+			{
+				ft_add_tetri(mat, t, point->x, point->y);
+				if (ft_put_tetri_mat(mat, t->next, dim_mat))
+					return (1);
+				else
+					ft_rm_tetri(mat, t, point->x, point->y);
+			}
+		}
+	}
+	if (t == NULL)
+		return (1);
+	else
+		return (0);
+}
